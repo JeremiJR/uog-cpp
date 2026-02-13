b91ca635b0d3fc1bce6dbcd25a0048c505bda36a
JeremiJR <jeremijanusz@gmail.com>
2026-02-13 01:14:03 +0000

    -update-

--- code/Rouge_room.cpp
+++ code/Rouge_room.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <string>
+
+void input()
+{
+    std::cout << "Input a move:W,A,S,D"; //Input by user for movement.
+    std::string input{};
+
+}
 
 int main(){
     std::cout << "------" << std::endl;
