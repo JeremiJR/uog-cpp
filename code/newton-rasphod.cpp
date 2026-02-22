15554c7f93ff41bba7d2e26c9b7d6efc292b15bc
root <root@Pip-Boy.localdomain>
2026-02-22 14:23:01 +0000

    --changes--

--- code/newton-rasphod.cpp
+++ code/newton-rasphod.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+
 using namespace std;
+// Fixed-point and Newton-Raphson methods for solving nonlinear equation
+// Fixed Point - Iteration method
+//void g(x) {
+ //   0.5 * (x + 5) / x
+//}
+//void fixed(){
+     
+//}
+
+
 int main() {
-    cout << "hello" << endl;
+    int x = 5+5 / 5
+    cout << x << endl;
 }
