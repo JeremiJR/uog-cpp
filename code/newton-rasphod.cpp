bf3ee19b234c8d0709bbe4c63e78b86537767571
root <root@Pip-Boy.localdomain>
2026-02-22 17:12:55 +0000

    --fixed point iteration--

--- code/newton-rasphod.cpp
+++ code/newton-rasphod.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 // Fixed-point and Newton-Raphson methods for solving nonlinear equation
 // Fixed Point - Iteration method
-//void g(x) {
- //   0.5 * (x + 5) / x
-//}
-//void fixed(){
-     
-//}
+double g(double y) {
+    return 0.5 * (y + 5) / y;
+    
+}
+
+void f(double x0){
+     double tol = 1e-6;
+     int n = 10000;
+     int nc = 0;
+     double xold = x0;
+     for (int k = 0; k < n; k++)
+        nc++;
+        double xnew = g(xold);
+        if (abs(xnew - xold) < tol) {
+            cout << "done at" << nc << endl;
+        }
+     xold = xnew;
+    
+}
 
 
 int main() {
-    int x = 5+5 / 5
-    cout << x << endl;
+    
+    f(2);
 }
