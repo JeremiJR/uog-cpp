f1d8d7fc0663e7caf3d55a0ffb2b448108071d3a
cafesua <khiem.nguyen@glasgow.ac.uk>
2026-03-19 11:44:40 +0000

    update entire chapter 15 in Section 03

--- sec-03-oop/15-virtual-functions/cpp/final_specifier_example.cpp
+++ sec-03-oop/15-virtual-functions/cpp/final_specifier_example.cpp
@@ -10,12 +10,12 @@ class B : public A
 {
 public:
 	// note use of final specifier on following line -- that makes this function not able to be overridden in derived classes
-	std::string_view get_name() const override final { return "B"; } // okay, overrides A::getName()
+	std::string_view get_name() const override final { return "B"; } // okay, overrides A::get_name()
 };
 
 class C : public B
 {
 public:
-    // compile error: overrides B::getName(), which is final
+    // compile error: overrides B::get_name(), which is final
 	// std::string_view get_name() const override { return "C"; }
 };
